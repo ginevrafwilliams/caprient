5dd5d63b07cf311987074f1c3f3db0b904f7608a
Ginevra Fontes Williams <ginevra.f.williams@gmail.com>
2020-02-25 20:10:01 +0100

    articles show mobile done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,5 +5,6 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @articles = Article.limit(3).order("RANDOM()").where.not(id: @article)
   end
 end
